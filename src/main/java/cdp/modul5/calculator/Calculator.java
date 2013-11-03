11ed7c9b784ac8360e05daa29354653ee090fa9e
Anton <acework@ukr.net>
2013-11-03 22:59:30 +0200

    initial commit

---
+++ src/main/java/cdp/modul5/calculator/Calculator.java
@@ -0,0 +1,123 @@
+package cdp.modul5.calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+
+    public float calculate(String input) {
+        input = input.replaceAll("\\s+", "");
+        if (validate(input)) {
+            List<String> parsed = parse(input);
+            float result = count(parsed);
+            return result;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean validate(String input) {
+        // TODO validation;
+        return true;
+    }
+
+    private List<String> parse(String input) {
+        List<String> stack = new ArrayList<String>();
+        List<String> parsed = new ArrayList<String>();
+        boolean digit = false;
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            if (Character.isDigit(ch)) {
+                if (digit) {
+                    String digitValue = parsed.get(parsed.size() - 1);
+                    digitValue = digitValue + ch;
+                    parsed.set(parsed.size() - 1, digitValue);
+                } else {
+                    parsed.add(String.valueOf(ch));
+                    digit = true;
+                }
+            } else if (isOperation(ch)) {
+                String newOperator = String.valueOf(ch);
+
+                if (stack.size() > 0) {
+
+//                    String last = stack.get(stack.size() - 1);
+
+                    if ((newOperator.equals("+") || newOperator.equals("-"))) {
+                        for (int j = stack.size() - 1; j >= 0; j--) {
+                            parsed.add(stack.get(j));
+                        }
+                        stack.clear();
+                    }
+                }
+                stack.add(newOperator);
+                digit = false;
+            } else {
+                throw new IllegalArgumentException();
+                // TODO remove it after validation creation
+
+            }
+        }
+        for (int i = stack.size() - 1; i >= 0; i--) {
+            parsed.add(stack.get(i));
+        }
+        System.out.println("parsed input: " + parsed.toString());
+
+        return parsed;
+    }
+
+    private boolean isOperation(char ch) {
+        if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isOperation(String s) {
+        if ("+".equals(s) || "-".equals(s) || "*".equals(s) || "/".equals(s)) {
+            return true;
+        }
+        return false;
+    }
+
+    private float count(List<String> parsed) {
+        float result = 0;
+        while (parsed.size() > 1) {
+            for (int i = 0; i < parsed.size(); i++) {
+                String element = parsed.get(i);
+                if (isOperation(element)) {
+                    float n1 = Float.valueOf(parsed.get(i - 2));
+                    float n2 = Float.valueOf(parsed.get(i - 1));
+
+                    if ("+".equals(element)) {
+                        result = n1 + n2;
+                        changeParsed(parsed, i, result);
+                        break;
+                    }
+                    if ("-".equals(element)) {
+                        result = n1 - n2;
+                        changeParsed(parsed, i, result);
+                        break;
+                    }
+                    if ("*".equals(element)) {
+                        result = n1 * n2;
+                        changeParsed(parsed, i, result);
+                        break;
+                    }
+                    if ("/".equals(element)) {
+                        result = n1 / n2;
+                        changeParsed(parsed, i, result);
+                        break;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    private void changeParsed(List<String> parsed, int i, float result) {
+        parsed.set(i - 2, String.valueOf(result));
+        parsed.remove(i - 1);
+        parsed.remove(i - 1);
+    }
+}
---
+++ src/main/java/cdp/modul5/calculator/Launcher.java
@@ -0,0 +1,34 @@
+package cdp.modul5.calculator;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+
+public class Launcher {
+    public static void main(String[] args) {
+//        String input = "3 * 2 / 6 - 2";  // -1
+//        String input = "2 * 3"; // 6
+//        String input = "2 + 3"; // 5
+//        String input = "2 + 3 + 1"; // 6
+//        String input = "2 + 3 - 1"; // 4
+//        String input = "2 + 3 * 4"; // 14
+//        String input = "2 * 3 + 4"; // 10
+//        String input = "2 * 3 / 4"; // 1.5
+//        String input = "2 * 3 - 1 / 4"; // 5.75
+//        String input = "2 - 1 * 3 - 1 / 4"; // -1.25
+//      String input = "2 * 3 - 1 + 5 / 4"; // 6.25
+//      String input = "6 - 1 + 2"; // 7
+  
+        String input = args[0];
+        Calculator calc = new Calculator();
+        float result = 0;
+            result = calc.calculate(input);
+
+            System.out.println("result : " + result);
+
+            DecimalFormatSymbols dfs = new DecimalFormatSymbols();
+            dfs.setDecimalSeparator('.');
+            DecimalFormat df = new DecimalFormat("#.00", dfs);
+
+            System.out.println("formatted result: " + df.format(result));
+    }
+}
